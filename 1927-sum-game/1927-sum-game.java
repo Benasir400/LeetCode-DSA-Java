00732f3ef9642b6a625bd0826e3c188f17c10749
Benasir_R <bena26875@gmail.com>
2026-08-23 21:32:04 +0530

    Time: 6 ms (97.08%), Space: 46.9 MB (64.23%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+
+        int s1 = 0, s2 = 0;
+        int cnt1 = 0, cnt2 = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            char c = num.charAt(i);
+            if (c == '?')
+                cnt1++;
+            else
+                s1 += c - '0';
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            char c = num.charAt(i);
+            if (c == '?')
+                cnt2++;
+            else
+                s2 += c - '0';
+        }
+
+        return ((cnt1 + cnt2) % 2 == 1) ||
+               (s1 - s2 != 9 * (cnt2 - cnt1) / 2);
+    }
+}
